3be39b9304ced4c07767dc23aeee965958172714
Christine Yoo <christinecho.s@gmail.com>
2021-06-18 13:56:24 -0700

    updated favicon

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -18,20 +18,34 @@ class Contact extends Component {
             would be great to hear from you. Please feel free to contact or
             follow me.
           </h2>
-          <form action='https://formspree.io/f/xdopyaql' method='POST'>
-            <label for='name'>Name</label>
-            <input id='form-name' type='text' name='name' required />
-            <br />
-            <label for='email'>Email</label>
-            <input id='form-email' type='text' name='email' required />
-            <br />
-            <label for='message'>Your message</label>
-            <textarea name='form-message' id='message' required></textarea>
-            <br />
-            <button id='form-button' type='submit'>
-              Submit
-            </button>
-          </form>
+          <div className='contact-buttons'>
+            <a href='mailto:christinecho.s@gmail.com'>
+              <button className='button-flex-1'>Email</button>
+            </a>
+            <a
+              href='https://docs.google.com/document/d/1uKNZGoU0uOLRKIDvVtgOIyZwP6GBBRyNO47CM_0wTpA/edit?usp=sharing'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <button className='button-flex-1'>Resume</button>
+            </a>
+          </div>
+          <div className='contact-buttons'>
+            <a
+              href='https://www.linkedin.com/in/christine-yoo-cy/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <button className='button-flex-1'>LinkedIn</button>
+            </a>
+            <a
+              href='https://github.com/christineyoo'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <button className='button-flex-1'>GitHub</button>
+            </a>
+          </div>
         </div>
         <button id='scroll-to-top' onClick={() => this.scrollToTop()}>
           Back to the top
--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,21 +6,6 @@ class Footer extends Component {
     return (
       <footer>
         <p>Copyright &copy; 2021 Christine Yoo</p>
-        <a
-          href='https://www.linkedin.com/in/christine-yoo-cy/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <i className='fab fa-linkedin-in'></i>
-        </a>
-        <br />
-        <a
-          href='https://github.com/christineyoo'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <i className='fab fa-github'></i>
-        </a>
       </footer>
     );
   }
