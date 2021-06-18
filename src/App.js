2be6f8f80ca3384839eb5a1d1f74e022cf829dad
Christine Yoo <christinecho.s@gmail.com>
2021-06-18 10:41:29 -0700

    added projects

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ class App extends Component {
       <>
         <header>
           <NavBar />
+          <Landing />
         </header>
         <main>
-          <Landing />
           <About />
           <Projects />
           <Contact />
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,20 +1,97 @@
 import React, { Component } from 'react';
+import './Projects.css';
 
 class Projects extends Component {
-  state = {};
   render() {
     return (
-      <>
-        <h1>PROJECTS</h1>
-        <h2>Sweet Bloom</h2>
-        <p>Description about sweet bloom</p>
-
-        <h2>Collab</h2>
-        <p>Description about Collab</p>
-
-        <h2>LUMINA</h2>
-        <p>Description about lumina</p>
-      </>
+      <div id='projects'>
+        <h1>Projects</h1>
+        <div className='projects-flex'>
+          <div className='projects-flex-1'>
+            <h3>some image</h3>
+          </div>
+          <div className='projects-flex-1'>
+            <h2>Sweet Bloom</h2>
+            <p>
+              Sweet Bloom is an e-commerce (business-to-consumer) application
+              that enables users to view products and read, create, update and
+              delete a review for any product. This app is for anyone who wants
+              to shop for flowers and desserts online. I built this app to
+              understand what goes behind building an e-commerce app, which is
+              ubiquitous in this day and age.
+            </p>
+            <p>
+              Skills/Tech used: HTML/CSS, Javascript, React, Node, Express,
+              PostgreSQL, Git, Vercel, Heroku, testing with Jest and Mocha/Chai
+            </p>
+            <a
+              href={'https://github.com/christineyoo/sweet-bloom'}
+              target={'_blank'}
+              rel={'noreferrer'}
+            >
+              GitHub repository here
+            </a>
+          </div>
+        </div>
+        <div className='projects-flex'>
+          <div className='projects-flex-1'>
+            <h3>some image</h3>
+          </div>
+          <div className='projects-flex-1'>
+            <h2>Collab</h2>
+            <p>
+              Collab is a productivity app that helps teachers increase their
+              efficacy by enabling intra- and inter-collaboration amongst course
+              teams. In a general sense, Collab could be used for members of
+              teams within a larger enterprise working towards a common goal.
+              Users are able to read, create, update, and delete a post and also
+              add or delete comments. The inspiration behind this app was to
+              create a means of communication that is more streamlined and
+              organized than traditional emails.
+            </p>
+            <p>
+              Skills/Tech used: HTML/CSS, Javascript, React, Gimp, Node,
+              Express, PostgreSQL, Git, Vercel, Heroku, testing with Jest and
+              Mocha/Chai
+            </p>
+            <a
+              href={'https://github.com/christineyoo/collab-app'}
+              target={'_blank'}
+              rel={'noreferrer'}
+            >
+              GitHub repository here
+            </a>
+          </div>
+        </div>
+        <div className='projects-flex'>
+          <div className='projects-flex-1'>
+            <h3>some image</h3>
+          </div>
+          <div className='projects-flex-1'>
+            <h2>LUMINA</h2>
+            <p>
+              This app enables anyone to see the Astronomy Picture of the Day
+              (APOD) and learn more about outer space by searching for terms in
+              the NASA image and video library. Users may simply search a term
+              of interest and scroll through all that there is to learn about
+              our fascinating universe! The purpose of this project was to
+              practice implementing asynchronous code by fetching data from two
+              different NASA API endpoints.
+            </p>
+            <p>
+              Skills/Tech used: HTML/CSS, JavaScript, jQuery, NASA APOD API,
+              NASA Image and Video Library API, Git
+            </p>
+            <a
+              href={'https://github.com/christineyoo/nasa-app'}
+              target={'_blank'}
+              rel={'noreferrer'}
+            >
+              GitHub repository here
+            </a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
