10e9c7c8175ef6cca418521e71f60bc61e752635
Christine Yoo <christinecho.s@gmail.com>
2021-06-18 13:28:20 -0700

    installed react-spring

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Landing.css';
+import { Spring } from 'react-spring';
 
-class Landing extends Component {
-  render() {
-    return (
-      <div id='landing'>
-        <div id='name'>
-          <h1 id='fade-in-h1'>christine yoo</h1>
-          <h2 id='fade-in-h2'>full stack developer</h2>
+export default function Landing() {
+  return (
+    <Spring
+      from={{ opacity: 0, marginTop: -500 }}
+      to={{ opacity: 1, marginTop: 0 }}
+    >
+      {(props) => (
+        <div id='landing' style={props}>
+          <div id='name'>
+            <h1 id='fade-in-h1'>christine yoo</h1>
+            <h2 id='fade-in-h2'>full stack developer</h2>
+          </div>
         </div>
-      </div>
-    );
-  }
+      )}
+    </Spring>
+  );
 }
-
-export default Landing;
