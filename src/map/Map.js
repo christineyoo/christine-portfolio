0b789351138740a2eaf31fcf29a9a589dc6cdf18
Christine Yoo <christinecho.s@gmail.com>
2021-06-18 13:10:16 -0700

    removed key

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 
 class Map extends Component {
-    
   render() {
     return (
       <>
         <script src='main.js'></script>
-        <script
-          async
-          defer
-          src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBCdo82fr1EGCM8YJT12zVwpwNNg9gSkxk&callback=initMap'
-        ></script>
+        <script async defer></script>
       </>
     );
   }
