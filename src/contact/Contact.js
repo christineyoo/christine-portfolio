20fa67f609cc9b9a47815aca2148ef39838c91af
Christine Yoo <christinecho.s@gmail.com>
2021-06-18 22:33:35 -0700

    re-added contact form

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import './Contact.css';
 
-class Contact extends Component {
-  scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  };
-  render() {
+export default function Contact() {
+  const [state, handleSubmit] = useForm('xdopyaql');
+
+  if (state.succeeded) {
     return (
       <>
         <div id='contact'>
@@ -19,9 +16,6 @@ class Contact extends Component {
             follow me.
           </h2>
           <div className='contact-buttons'>
-            <a href='mailto:christinecho.s@gmail.com'>
-              <button className='button-flex-1'>Email</button>
-            </a>
             <a
               href='https://docs.google.com/document/d/1uKNZGoU0uOLRKIDvVtgOIyZwP6GBBRyNO47CM_0wTpA/edit?usp=sharing'
               target='_blank'
@@ -29,8 +23,6 @@ class Contact extends Component {
             >
               <button className='button-flex-1'>Resume</button>
             </a>
-          </div>
-          <div className='contact-buttons'>
             <a
               href='https://www.linkedin.com/in/christine-yoo-cy/'
               target='_blank'
@@ -46,13 +38,88 @@ class Contact extends Component {
               <button className='button-flex-1'>GitHub</button>
             </a>
           </div>
+          <h1>Thanks, your message was sent!</h1>
         </div>
-        <button id='scroll-to-top' onClick={() => this.scrollToTop()}>
+        <button
+          id='scroll-to-top'
+          onClick={() =>
+            window.scrollTo({
+              top: 0,
+              behavior: 'smooth'
+            })
+          }
+        >
           Back to the top
         </button>
       </>
     );
   }
-}
+  return (
+    <>
+      <div id='contact'>
+        <h1>contact</h1>
+        <h2>
+          If you're looking to hire a new developer or just want to chat, it
+          would be great to hear from you. Please feel free to contact or follow
+          me.
+        </h2>
+        <div className='contact-buttons'>
+          <a
+            href='https://docs.google.com/document/d/1uKNZGoU0uOLRKIDvVtgOIyZwP6GBBRyNO47CM_0wTpA/edit?usp=sharing'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='button-flex-1'>Resume</button>
+          </a>
+          <a
+            href='https://www.linkedin.com/in/christine-yoo-cy/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='button-flex-1'>LinkedIn</button>
+          </a>
+          <a
+            href='https://github.com/christineyoo'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='button-flex-1'>GitHub</button>
+          </a>
+        </div>
 
-export default Contact;
+        <form onSubmit={handleSubmit}>
+          <label for='name'>Your name</label>
+          <input id='form-name' type='text' name='name' required />
+          <ValidationError prefix='name' field='name' errors={state.errors} />
+          <br />
+          <label for='email'>Your email</label>
+          <input id='email' type='email' name='email' required />
+          <ValidationError prefix='email' field='email' errors={state.errors} />
+          <br />
+          <label for='message'>Your message</label>
+          <textarea name='message' id='message' required />
+          <ValidationError
+            prefix='message'
+            field='message'
+            errors={state.errors}
+          />
+          <br />
+          <button type='submit' disabled={state.submitting}>
+            Submit
+          </button>
+        </form>
+      </div>
+      <button
+        id='scroll-to-top'
+        onClick={() =>
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          })
+        }
+      >
+        Back to the top
+      </button>
+    </>
+  );
+}
