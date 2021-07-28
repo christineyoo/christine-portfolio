e6ed9cea513a97311ee07b66148d996f592d9114
Christine Yoo <christinecho.s@gmail.com>
2021-07-27 23:20:41 -0700

    updated about section

--- src/about/About.js
+++ src/about/About.js
@@ -7,23 +7,21 @@ class About extends Component {
       <div id='about'>
         <h1>Hi, I'm Christine.</h1>
         <p>
-          I'm a full stack web developer located in Redondo Beach, CA with four
-          years of experience teaching high school math. I recently graduated
+          I'm a full stack web developer with four
+          years of experience teaching high school math. I graduated
           from{' '}
           <a href='https://www.thinkful.com/' target='_blank' rel='noreferrer'>
             Thinkful's Software Engineering program
           </a>{' '}
-          where I studied web development using HTML/CSS, React, Node, Express,
-          and PostgreSQL.
+          where I studied web development using JavaScript, HTML/CSS, React, Node, Express, SQL,
+          and PostgreSQL. I am currently extending my education by learning about AWS.
         </p>
         <p>
           In addition to those skills, my previous work in education gives me an
           extra edge when it comes to developing solutions within a team since I
-          learned the importance of setting a game plan beforehand, adjusting
+          learned the importance of setting a game plan before execution, adjusting
           based on new developments, and streamlining communication amongst
-          stakeholders. When I'm not in front of my computer, I like to explore
-          the beautiful South Bay area with my husband and my energetic
-          Schnoodle, Fluffy.
+          stakeholders. When I'm not in front of my computer, I like to explore California's beautiful coast with my husband and energetic Schnoodle, Fluffy.
         </p>
       </div>
     );
